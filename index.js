5dbb18df82e1b10d06b49726972224dc5008a214
Alexis Abril <me@alexisabril.com>
2015-08-19 11:49:26 -0500

    better check for can & viewmodel existing

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var getViewModel = function() {
 	var can = window.can;
+	var viewModel = can
+		? $0 && can.viewModel($0)
+		: null;
 
-	if(can) {
-		var viewModel = $0 && can.viewModel($0);
-
-		//TODO: invoke updateElementProperties when viewModel changes
-
+	if(viewModel) {
 		var data = viewModel.serialize();
 		data.__proto__ = null;
+
 		return data;
 	}
 };
 
-chrome.devtools.panels.elements.createSidebarPane('CanJS ViewModel',
-	function(sidebar) {
-		function updateElementProperties() {
-			sidebar.setExpression("(" + getViewModel.toString() + ")()");
-		}
+chrome.devtools
+	.panels.elements
+	.createSidebarPane('CanJS', function(sidebar) {
+	var updateElementProperties = function() {
+		sidebar.setExpression('(' + getViewModel.toString() + ')()', 'ViewModel');
+	};
 
-		updateElementProperties();
+	updateElementProperties();
 
-		chrome.devtools
-			.panels.elements
-			.onSelectionChanged
-			.addListener(updateElementProperties);
-	}
-);
+	chrome.devtools
+		.panels.elements
+		.onSelectionChanged
+		.addListener(updateElementProperties);
+});
